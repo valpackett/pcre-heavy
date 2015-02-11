b20900c5466604a48290e88f08eb9b1a1d5de6aa
Greg V <greg@unrelenting.technology>
2015-02-11 17:19:03 +0300

    add sub, gsub...

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # pcre-heavy [![Hackage](https://img.shields.io/hackage/v/pcre-heavy.svg?style=flat)](https://hackage.haskell.org/package/pcre-heavy) [![ISC License](https://img.shields.io/badge/license-ISC-red.svg?style=flat)](https://tldrlegal.com/license/-isc-license)
 
-**Finally!** A Haskell regular expressions library that does not suck.
+*Finally!* A Haskell regular expressions library that does not suck.
 
 - based on [pcre-light], none of that regex-compat-pluggable-backend stuff
 - takes [Stringables] everywhere, use ANY STRING TYPE (String, ByteString, LByteString, Text, LText, FilePath)
 - a QuasiQuoter for regexps that does compile time checking (BTW, [vim2hs] has correct syntax highlighting for that!)
+- **SEARCHES FOR MULTIPLE MATCHES! DOES REPLACEMENT!**
 
 [pcre-light]: https://hackage.haskell.org/package/pcre-light
 [Stringables]: https://hackage.haskell.org/package/stringable
@@ -15,25 +16,45 @@
 ```haskell
 {-# LANGUAGE QuasiQuotes #-}
 import           Text.Regex.PCRE.Heavy
+```
 
--- Checking:
-let isUrl = "https://unrelenting.technology" =~ [re|^http.*|] :: Bool
+### Matching
 
--- In an `if`, you don't even need the annotation:
-putStrLn $ if "https://unrelenting.technology" =~ [re|^http.*|] then "YEP" else "NOPE"
+```haskell
+>>> "https://unrelenting.technology" =~ [re|^http.*|] :: Bool
+True
+```
 
--- Extracting matches: (Note: You can use any string type, not just String)
-let domain = "https://unrelenting.technology" =~ [re|^https?://([^\.]+)\..*|] :: Maybe [String]
--- returns
+In an `if`, you don't even need the annotation:
+
+```haskell
+if "https://unrelenting.technology" =~ [re|^http.*|] then "YEP" else "NOPE"
+```
+
+Extracting matches (You can use any string type, not just String)
+
+```haskell
+>>> "https://unrelenting.technology" =~ [re|^https?://([^\.]+)\..*|] :: Maybe [String]
 Just ["https://unrelenting.technology","unrelenting"]
+```
 
--- Multiple matches (also called search, scan):
-let entries = scan [re|\s*entry (\d+) (\w+)\s*&?|] " entry 1 hello  &entry 2 hi" :: [[String]]
--- returns
+`scan` returns all matches (search)
+
+```haskell
+>>> scan [re|\s*entry (\d+) (\w+)\s*&?|] " entry 1 hello  &entry 2 hi" :: [[String]]
 [
   [" entry 1 hello  &", "1", "hello"]
 , ["entry 2 hi",        "2", "hi"]
 ]
+```
+
+### Replacement
+
+`sub` replaces the first match, `gsub` replaces all matches.
+
+```haskell
+>>> gsub [re|%(\d+)(\w+)|] (\(d:w:_) -> "{" ++ d ++ " of " ++ w ++ "}" :: String) "Hello, %20thing" :: String
+"Hello, {20 of thing}"
 ```
 
 ## License
--- library/Text/Regex/PCRE/Heavy.hs
+++ library/Text/Regex/PCRE/Heavy.hs
@@ -1,35 +1,43 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-unused-binds #-}
+{-# LANGUAGE UndecidableInstances, OverlappingInstances #-}
 {-# LANGUAGE FlexibleInstances, BangPatterns #-}
-{-# LANGUAGE UndecidableInstances, ForeignFunctionInterface #-}
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
+-- | A usable regular expressions library on top of pcre-light.
 module Text.Regex.PCRE.Heavy (
+  -- * Matching
   (=~)
 , scan
+, scanO
+  -- * Replacement
+, sub
+, subO
+, gsub
+, gsubO
+  -- * QuasiQuoter
 , re
+, mkRegexQQ
+  -- * Types from pcre-light
 , Regex
 , PCREOption
+  -- * Advanced raw stuff
 , rawMatch
+, rawSub
 ) where
 
 import           Language.Haskell.TH hiding (match)
 import           Language.Haskell.TH.Quote
 import           Language.Haskell.TH.Syntax
 import qualified Text.Regex.PCRE.Light as PCRE
-import           Text.Regex.PCRE.Light (Regex, PCREOption)
 import           Text.Regex.PCRE.Light.Base
 import           Control.Applicative ((<$>))
-import           Data.Maybe (isJust)
+import           Data.Maybe (isJust, fromMaybe)
 import           Data.Stringable
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Internal as BS
 import           System.IO.Unsafe (unsafePerformIO)
 import           Foreign
-import           Foreign.Ptr
-import           Foreign.C.Types
-import           Foreign.C.String
-import           Foreign.Storable
-import           Foreign.Marshal.Alloc
 
 class RegexResult a where
   fromResult :: Maybe [BS.ByteString] -> a
@@ -49,9 +57,8 @@ reMatch r s = fromResult $ PCRE.match r (toByteString s) []
 -- | Matches a string with a regex.
 --
 -- You can cast the result to Bool or Maybe [Stringable]
--- (where Stringable is anything supported by Data.Stringable).
 -- Maybe [Stringable] only represents the first match and its groups.
--- Use scan to find all matches.
+-- Use 'scan' to find all matches.
 --
 -- Note: if casts to bool automatically.
 --
@@ -67,46 +74,117 @@ reMatch r s = fromResult $ PCRE.match r (toByteString s) []
 
 -- | Does raw PCRE matching (you probably shouldn't use this directly).
 -- 
+-- >>> :set -XOverloadedStrings
 -- >>> rawMatch [re|\w{2}|] "a a ab abc ba" 0 []
 -- Just [(4,6)]
 -- >>> rawMatch [re|\w{2}|] "a a ab abc ba" 6 []
 -- Just [(7,9)]
 -- >>> rawMatch [re|(\w)(\w)|] "a a ab abc ba" 0 []
 -- Just [(4,6),(4,5),(5,6)]
-rawMatch :: (Stringable a) => Regex -> a -> Int -> [PCREExecOption] -> Maybe [(Int, Int)]
+rawMatch :: Regex -> BS.ByteString -> Int -> [PCREExecOption] -> Maybe [(Int, Int)]
 rawMatch r@(Regex pcreFp _) s offset opts = unsafePerformIO $ do
   withForeignPtr pcreFp $ \pcrePtr -> do
     let nCapt = PCRE.captureCount r
         ovecSize = (nCapt + 1) * 3
         ovecBytes = ovecSize * size_of_cint
     allocaBytes ovecBytes $ \ovec -> do
-      let (strFp, off, len) = BS.toForeignPtr $ toByteString s
+      let (strFp, off, len) = BS.toForeignPtr s
       withForeignPtr strFp $ \strPtr -> do
-        r <- c_pcre_exec pcrePtr nullPtr (strPtr `plusPtr` off) (fromIntegral len) (fromIntegral offset)
-                         (combineExecOptions opts) ovec (fromIntegral ovecSize)
-        if r < 0 then return Nothing
+        results <- c_pcre_exec pcrePtr nullPtr (strPtr `plusPtr` off) (fromIntegral len) (fromIntegral offset)
+                               (combineExecOptions opts) ovec (fromIntegral ovecSize)
+        if results < 0 then return Nothing
         else
           let loop n o acc =
-                if n == r then return $ Just $ reverse acc
+                if n == results then return $ Just $ reverse acc
                 else do
                   i <- peekElemOff ovec $! o
                   j <- peekElemOff ovec (o + 1)
                   loop (n + 1) (o + 2) ((fromIntegral i, fromIntegral j) : acc)
           in loop 0 0 []
 
+substr :: BS.ByteString -> (Int, Int) -> BS.ByteString
+substr s (f, t) = BS.take (t - f) . BS.drop f $ s
+
 -- | Searches the string for all matches of a given regex.
 --
 -- >>> scan [re|\s*entry (\d+) (\w+)\s*&?|] " entry 1 hello  &entry 2 hi"
 -- [[" entry 1 hello  &","1","hello"],["entry 2 hi","2","hi"]]
 scan :: (Stringable a) => Regex -> a -> [[a]]
-scan r s = map fromByteString <$> loop 0 []
+scan r s = scanO r [] s
+
+-- | Exactly like 'scan', but passes runtime options to PCRE.
+scanO :: (Stringable a) => Regex -> [PCREExecOption] -> a -> [[a]]
+scanO r opts s = map fromByteString <$> loop 0 []
   where str = toByteString s
         loop offset acc =
-          case rawMatch r str offset [] of
+          case rawMatch r str offset opts of
             Nothing -> reverse acc
             Just [] -> reverse acc
-            Just ms -> loop (maximum $ map snd ms) (getMatches ms : acc)
-        getMatches ms = map (\(f, t) -> BS.take (t - f) $ BS.drop f str) ms
+            Just ms -> loop (maximum $ map snd ms) ((map (substr str) ms) : acc)
+
+class RegexReplacement a where
+  performReplacement :: BS.ByteString -> [BS.ByteString] -> a -> BS.ByteString
+
+instance Stringable a => RegexReplacement a where
+  performReplacement _ _ to = toByteString to
+
+instance Stringable a => RegexReplacement (a -> [a] -> a) where
+  performReplacement from groups replacer = toByteString $ replacer (fromByteString from) (map fromByteString groups)
+
+instance Stringable a => RegexReplacement (a -> a) where
+  performReplacement from _ replacer = toByteString $ replacer (fromByteString from)
+
+instance Stringable a => RegexReplacement ([a] -> a) where
+  performReplacement _ groups replacer = toByteString $ replacer (map fromByteString groups)
+
+rawSub :: RegexReplacement r => Regex -> r -> BS.ByteString -> Int -> [PCREExecOption] -> Maybe (BS.ByteString, Int)
+rawSub r t s offset opts =
+  case rawMatch r s offset opts of
+    Just ((begin, end):groups) ->
+      Just (BS.concat [ substr s (0, begin)
+                      , performReplacement (substr s (begin, end)) (map (substr s) groups) t
+                      , substr s (end, BS.length s)], end)
+    _ -> Nothing
+
+-- | Replaces the first occurence of a given regex.
+--
+-- >>> sub [re|thing|] "world" "Hello, thing thing" :: String
+-- "Hello, world thing"
+--
+-- >>> sub [re|a|] "b" "c" :: String
+-- "c"
+--
+-- You can use functions!
+-- A function of Stringable gets the full match.
+-- A function of [Stringable] gets the groups.
+-- A function of Stringable -> [Stringable] gets both.
+--
+-- >>> sub [re|%(\d+)(\w+)|] (\(d:w:_) -> "{" ++ d ++ " of " ++ w ++ "}" :: String) "Hello, %20thing" :: String
+-- "Hello, {20 of thing}"
+sub :: (Stringable a, RegexReplacement r) => Regex -> r -> a -> a
+sub r t s = subO r [] t s
+
+-- | Exactly like 'sub', but passes runtime options to PCRE.
+subO :: (Stringable a, RegexReplacement r) => Regex -> [PCREExecOption] -> r -> a -> a
+subO r opts t s = fromMaybe s $ fromByteString <$> fst <$> rawSub r t (toByteString s) 0 opts
+
+-- | Replaces all occurences of a given regex.
+--
+-- See 'sub' for more documentation.
+--
+-- >>> gsub [re|thing|] "world" "Hello, thing thing" :: String
+-- "Hello, world world"
+gsub :: (Stringable a, RegexReplacement r) => Regex -> r -> a -> a
+gsub r t s = gsubO r [] t s
+
+-- | Exactly like 'gsub', but passes runtime options to PCRE.
+gsubO :: (Stringable a, RegexReplacement r) => Regex -> [PCREExecOption] -> r -> a -> a
+gsubO r opts t s = fromByteString $ loop 0 str
+  where str = toByteString s
+        loop offset acc =
+          case rawSub r t acc offset opts of
+            Just (result, newOffset) -> loop newOffset result
+            _ -> acc
 
 instance Lift PCREOption where
   lift o = [| o |]
@@ -115,6 +193,7 @@ quoteExpRegex :: [PCREOption] -> String -> ExpQ
 quoteExpRegex opts txt = [| PCRE.compile (BS.pack txt) opts |]
   where !_ = PCRE.compile (BS.pack txt) opts -- check at compile time
 
+-- | Returns a QuasiQuoter like 're', but with given PCRE options.
 mkRegexQQ :: [PCREOption] -> QuasiQuoter
 mkRegexQQ opts = QuasiQuoter
   { quoteExp  = quoteExpRegex opts
